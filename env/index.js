eaf6718aa36c19d9faf17be94379919653008d83
Michael Bruns <michaelbruns99@gmail.com>
2022-08-16 01:00:37 -0400

    config updates

---
+++ env/index.js
@@ -0,0 +1,11 @@
+const APP_ID = process.env.APP_ID || "";
+const GUILD_ID = process.env.GUILD_ID || "";
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN || "";
+const PUBLIC_KEY = process.env.PUBLIC_KEY || "";
+
+module.exports = {
+  APP_ID,
+  GUILD_ID,
+  DISCORD_TOKEN,
+  PUBLIC_KEY,
+};
